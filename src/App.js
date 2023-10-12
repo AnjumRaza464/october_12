4fecf67e99b7e429f2a697c6810ad790dfe51f10
AnjumRazaNexusberry <hafizanjumraza1993@gmail.com>
2023-10-12 20:21:50 +0500

    first

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Layout from './Layout/Main'
+
+const App = () => {
+  return (
+    <>
+    
+    <Layout>
+      This is a Body
+    </Layout>
+    
+    </>
+  )
+}
+
+export default App
---
+++ src/component/Index.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from './header'
+import Footer from './footer'
+
+const Layout = () => {
+  return (
+    <>
+    <Header/>
+<h1>This is index</h1>
+
+    <Footer/>
+    </>
+  )
+}
+
+export default Layout
